580800c6b6a2fc916250d58009dab67cd3156679
Yann Hodique <yann.hodique@gmail.com>
2013-03-04 21:23:53 +0000

    rebrand as "codex"

--- README.md
+++ README.md
@@ -12,15 +12,24 @@ This is an attempt at namespacing Elisp symbols.
 Contrary to other implementations (like
 https://github.com/skeeto/elisp-fakespace), we don't constrain ourselves to the
 main obarray. We instead try to create the symbols at the right place directly,
-which basically means in the obarray that's associated with each package.
+which basically means in the obarray that's associated with each codex.
 
-The core of the implementation is the `in-package` macro, which rewrites its
+The core of the implementation is the `in-codex` macro, which rewrites its
 body's symbols to point at the right places.
-In particular, a symbol name of the form "<package>:<symbol>" will be resolved
-as the symbol "symbol" in package "package", which is *not* backward-compatible.
+In particular, a symbol name of the form "<codex>:<symbol>" will be resolved
+as the symbol "symbol" in codex "codex", which is *not* backward-compatible.
 
-Existing packages
-=================
+Why Codex ?
+===========
+
+Well, this is modeled after CL packages, but packages are something entirely
+different in Emacs. I thought of "thesaurus" first, but I can't be bothered to
+type this all the time. "lexicon" sounded nice, but this might be confusing
+with all the lexical binding stuff that's happening right now.
+"codex" is short and still refers to a collection of symbols :)
+
+Existing codices
+================
 
 One of the challenges of introducing a proper namespacing mechanism is to
 decide what to do with all the existing stuff in the global obarray.
@@ -30,7 +39,7 @@ contains every built-in function defined in emacs. That's certainly not
 sufficient, but is enough for a small POC.
 
 If this is deemed worth, one could plug into `after-load-functions`, and parse
-`load-history` to generate packages from existing library, so that they could
+`load-history` to generate codices from existing library, so that they could
 at least be properly integrated.
 
 Examples explained
@@ -40,11 +49,11 @@ For clarity, we'll use the colon notation to qualify the symbols, but that's
 obviously not what one would observe, as the symbol names don't contain
 that qualification.
 
-    (epack-defpackage test
+    (defcodex test
       (:use emacs)
       (:export "plop"))
     
-    (epack-in-package test
+    (in-codex test
       (defun plop () 42))
 
 The defun form will be transformed into:
@@ -52,35 +61,35 @@ The defun form will be transformed into:
     (emacs:defun test:plop nil 42)
 
 That means, the `defun` symbol has been detected as exported by the "emacs"
-package, while `plop` was (probably) not even existing, so that it ends up in
+codex, while `plop` was (probably) not even existing, so that it ends up in
 the "test" one.
 
-If we didn't have the `(:use emacs)` clause in the package definition, the
+If we didn't have the `(:use emacs)` clause in the codex definition, the
 expansion would have been
 
     (test:defun test:plop nil 42)
 
-This allows to easily overload functions in packages (note that when proper
+This allows to easily overload functions in codex (note that when proper
 shadowing is in place, it will actually become useful).
 
 Another example:
 
-    (epack-defpackage test2)
+    (defcodex test2)
     
-    (epack-in-package test2
+    (in-codex test2
       (emacs:defun plop () 7))
 
-This time, no use of emacs package, so we need to prefix the `defun` symbol to
+This time, no use of emacs codex, so we need to prefix the `defun` symbol to
 refer to it. The defun form is transformed into:
 
     (emacs:defun test:plop nil 7)
 
 Finally:
 
-    (epack-defpackage test3
+    (defcodex test3
       (:use test))
     
-    (epack-in-package test3
+    (in-codex test3
       (emacs:+ (plop) (test2:plop)))
 
 The addition form becomes:
@@ -97,10 +106,10 @@ on something that has already gone through the `read` phase, meaning that all
 referenced symbols are interned in the global obarray anyway. That's unclean.
 
 To be clear, that doesn't mean the symbols in the main obarray have the
-function or value slots of their packages counterparts. It's just that
+function or value slots of their codex counterparts. It's just that
 they exist, in a (mostly) harmless state.
 
-Debugging code that's using those packages would quickly become a nightmare in
+Debugging code that's using those codices would quickly become a nightmare in
 the absence of proper edebug integration. In particular, the fact that symbols
 are not prefixed makes it very difficult to know what symbol we're
 talking about.
---
+++ codex.el
@@ -0,0 +1,155 @@
+;;; codex.el --- cl-like namespaces for emacs
+
+;; Copyright (C) 2013  Yann Hodique
+
+;; Author: Yann Hodique <yann.hodique@gmail.com>
+;; Keywords: lisp
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; This code is experimental, and is not meant to be used in any significant
+;; piece of software for now.
+
+;;; Code:
+
+(eval-when-compile
+  (require 'cl))
+
+(defstruct (codex-struct :named)
+  used
+  symbols
+  export)
+
+(defvar codex-structs nil)
+
+(defun codex-string-id (name)
+  (or (and (stringp name) name)
+      (and (symbolp name) (symbol-name name))))
+
+(defun codex-define (name specs)
+  (let* ((codex (make-codex-struct))
+         (used (cdr (assoc :use specs)))
+         (export (cdr (assoc :export specs))))
+    (setf (codex-struct-used codex)
+          (mapcar 'codex-string-id used))
+    (setf (codex-struct-export codex)
+          (mapcar 'codex-string-id export))
+    (setf (codex-struct-symbols codex)
+          (make-vector 17 nil))
+    (let ((cod (assoc name codex-structs)))
+      (if cod
+          (setcdr cod codex)
+        (push (cons name codex) codex-structs))
+      codex)))
+
+(defun codex-find-symbol--deps (name deps)
+  (and deps
+       (or
+        (let* ((dep (car deps))
+               (cod (cdr (assoc dep codex-structs))))
+          (and cod
+               (member name (codex-struct-export cod))
+               (intern-soft name (codex-struct-symbols cod))))
+        (codex-find-symbol--deps name (cdr deps)))))
+
+(defun codex-find-symbol (name cod)
+  (and cod
+       (or (codex-find-symbol--deps name (codex-struct-used cod))
+           (intern-soft name (codex-struct-symbols cod)))))
+
+(defun codex-intern (name cod)
+  (or (codex-find-symbol name cod)
+      (intern name (codex-struct-symbols cod))))
+
+(defun codex-resolve-symbol (sym default-codex)
+  (let ((name (symbol-name sym)))
+    (cond ((string-match-p "^:.*" name)
+           sym)
+          ((and (string-match "^\\(.*\\):\\(.*\\)" name)
+                (assoc (match-string 1 name) codex-structs)
+                (codex-find-symbol (match-string 2 name)
+                                   (cdr (assoc (match-string 1 name)
+                                               codex-structs)))))
+          (t (codex-intern name default-codex)))))
+
+(defun codex-in-codex (codname forms)
+  (let* ((name (codex-string-id codname))
+         (cod (cdr (assoc name codex-structs))))
+    (mapcar (lambda (form)
+              (cond ((null form) nil)
+                    ((symbolp form)
+                     (codex-resolve-symbol form cod))
+                    ((listp form)
+                     (codex-in-codex name form))
+                    (t form)))
+            forms)))
+
+;;;###autoload
+(defmacro defcodex (name &rest specs)
+  (declare (indent 1))
+  (let ((name (codex-string-id name)))
+    `(codex-define ,name ',specs)))
+
+;;;###autoload
+(defmacro in-codex (codname &rest body)
+  (declare (indent 1))
+  (cons 'progn
+        (codex-in-codex codname `,body)))
+
+(defmacro codex-initialize ()
+  '(progn
+     ;; initialize the "emacs" codex, with all builtins
+     (let ((emacs (defcodex emacs)))
+       (setf (codex-struct-symbols emacs) obarray)
+       (setf (codex-struct-export emacs)
+             (let ((subrs nil))
+               (mapatoms (lambda (s)
+                           (when (and (fboundp s) (subrp (symbol-function s)))
+                             (setq subrs (cons (symbol-name s) subrs)))))
+               subrs)))))
+
+(provide 'codex)
+
+(eval-after-load 'codex
+  '(codex-initialize))
+
+;; (defcodex test
+;;   (:use emacs)
+;;   (:export "plop"))
+
+;; (in-codex test
+;;   (defun plop () 42))
+
+;; (defcodex test2)
+
+;; (in-codex test2
+;;   (emacs:defun plop () 0))
+
+;; (in-codex test
+;;   (plop))
+
+;; (in-codex test2
+;;   (plop))
+
+;; (defcodex test3
+;;   (:use test))
+
+;; (in-codex test3
+;;   (plop))
+
+;;; codex.el ends here
